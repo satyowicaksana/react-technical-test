a5698c612a7eeca4a6304f1cef765c9c3983f9e1
Satyo Wicaksana <satyowicaksana@gmail.com>
2020-01-02 15:01:18 +0700

    display repo list

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Form, Icon, Input, Button } from 'antd';
+import { Form, Icon, Input, Button, List } from 'antd';
 import { fetchRepos } from '../store/actions'
 import { useDispatch, useSelector } from 'react-redux'
+import './Home.css'
 
 export default () => {
   const [username, setUsername] = useState('')
@@ -16,23 +17,39 @@ export default () => {
 
   return (
     <>
-      <p>this is home</p>
-      <Form layout="inline" onSubmit={handleSubmit}>
-        <Form.Item>
-          <Input
-            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-            placeholder="Username"
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Show Repos
-          </Button>
-        </Form.Item>
-      </Form>
-      <p>{JSON.stringify(repos)}</p>
+      <div className="container">
+        <Form layout="inline" onSubmit={handleSubmit}>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Show Repos
+            </Button>
+          </Form.Item>
+        </Form>
+        <List
+          style={{textAlign: 'left'}}
+          itemLayout="horizontal"
+          dataSource={repos}
+          pagination={{
+            pageSize: 10,
+          }}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                title={<b><a href="https://ant.design">{item.name}</a></b>}
+                description={item.description}
+              />
+            </List.Item>
+          )}
+        />
+      </div>
     </>
   )
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,5 @@
 export const fetchRepos = (username) => async dispatch => {
-  const response = await fetch(`https://api.github.com/users/${username}/repos`)
+  const response = await fetch(`https://api.github.com/users/${username}/repos?sort=created`)
   const repos = await response.json()
   dispatch({
     type: 'SET_REPOS',
